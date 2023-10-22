987abd3c0cc78a5d56ba167413a996d942166c4e
Kirshmakumari <kumarikirshma@gmail.com>
2023-10-22 20:37:46 +0500

    added animation-toggle menu btn-backtoTop btn

--- script.js
+++ script.js
@@ -1,21 +1,102 @@
-// typing animation script
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("show");
+    $(".menu-btn i").toggleClass("show");
+  });
+  
+   $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
+
+  
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+// typing text animation script
 var typed = new Typed(".typing", {
-    strings: ["Front-End Developer", "UI Designer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+  strings: ["Front-End Developer", "UI Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
 
 var typed = new Typed(".typing-2", {
-    strings: ["Frontend Developer" ,"BSCS Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+  strings: ["Frontend Developer" ,"BSCS Student"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
-// menu icon
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+
+const sr = ScrollReveal({
+  distance: '80px',
+  duration: 2800,
+  reset: true
+})
+sr.reveal('.home-content', { delay: 300, origin: 'bottom' });
+sr.reveal('nav', { delay: 100, origin: 'top' })
+sr.reveal('section , .text', { delay: 300, origin: 'bottom' });
+sr.reveal('footer', { delay: 300, origin: 'left' });
+
+const sr2 = ScrollReveal({
+  distance: '80px',
+  duration: 1000,
+  reset: false
+})
+sr2.reveal('.line', { delay: 50, origin: 'left' })
+
+
+
+
